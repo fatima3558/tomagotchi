720921f288a5c25624429e638e1f5cd8e307f389
Fatima Gomez <fatima3558@gmail.com>
2019-07-03 09:26:11 -0500

    pseudocoded

--- js/Tomagotchi.js
+++ js/Tomagotchi.js
@@ -6,26 +6,32 @@ class Tomagotchi {
 		this.hunger = 0
 		this.sleepiness = 0
 		this.boredom = 0
+		this.timesBorn = 0
+	}
+
+	instEgg() {
+		//after x minutes, become baby
 	}
 
 	becomeBaby() {
-
+		//after x minutes, become Child
 	}
 
 	becomeChild() {
-
+		//after x minutes, become Adult
 	}
 
 	becomeAdult() {
-
+		//after x minutes, be Reborn
 	}
 
 	beReborn() {
-
+		//burn up, be reborn from ashes as baby
 	}
 
 	die() {
-
+		//if hunger OR boredom OR sleepiness hits 10, stop timer
+		//display time of death
 	}
 }
 
--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 console.log("tomagotchi");
 
 const game = {
+
 	createTomagotchi() {
+		//add event listener for button id = 'newTomagotchi'
 		const name = prompt("Name your Tomagotchi!");
 		console.log(name);
 		return name;
+		//hide button
+		//start timer
+	}
+
+	startTimer() {
+		//pause button
+	}
+
+	increaseBoredom() {
+		//increase by 1 every 10 min
+	}
+
+	increaseHunger() {
+		//increase by 1 every 20 min
+	}
+
+	increaseSleepiness() {
+		//Increase by 1 every 30 min
 	}
 }
+
 
 game.createTomagotchi();
